fa95f79d76d8ca4c210fd4d2f020dc3c47de3cf0
Wai Yan Min Khaing <waiyanminkhaing1997@gmail.com>
2025-05-05 16:42:22 +0900

    save plausible translate

--- script/squad/plausible_translate.py
+++ script/squad/plausible_translate.py
@@ -7,41 +7,70 @@ import pyautogui
 import pyperclip
 
 
-def upload_file(is_first, filename):
+def copy(y):
+    pyperclip.copy(f"Error: {filename}\n")
+    pyautogui.moveTo(950, y, duration=0.5)
+    pyautogui.click()
+    pyautogui.click()
+
+
+def upload_file(is_first):
     pyautogui.moveTo(460, 880, duration=0.5)
     if is_first:
         pyautogui.click()
     pyautogui.click()
-    time.sleep(10)
+    time.sleep(2)
 
     pyautogui.moveTo(500, 840, duration=0.5)
     pyautogui.click()
-    time.sleep(10)
+    time.sleep(2)
 
     pyautogui.moveTo(1000, 320, duration=0.5)
     pyautogui.click()
-    time.sleep(10)
+    time.sleep(2)
 
     pyautogui.moveTo(1120, 710, duration=0.5)
     pyautogui.click()
-    time.sleep(60)
+    time.sleep(5)
 
     pyautogui.moveTo(1030, 890, duration=0.5)
     pyautogui.click()
     pyautogui.moveTo(1100, 890, duration=0.5)
-    time.sleep(240)
+    time.sleep(90)
 
-    pyperclip.copy(f"Error: {filename}\n")
-    pyautogui.moveTo(950, 417, duration=0.5)
+
+def fill_file(is_first, filename):
+    pyautogui.moveTo(470, 840, duration=0.5)
+    if is_first:
+        pyautogui.click()
     pyautogui.click()
+
+    dest_path = os.path.join(destination_folder, filename)
+    with open(dest_path, "r", encoding="utf-8") as file:
+        content = file.read()
+        data = f'"""\n{content}\n"""\nI want all 10 rows in json for {filename}'
+        pyperclip.copy(data)
+        pyautogui.hotkey("command", "v")
+
+    pyautogui.moveTo(1030, 890, duration=0.5)
     pyautogui.click()
-    time.sleep(30)
+    pyautogui.moveTo(1100, 890, duration=0.5)
+    time.sleep(90)
 
 
 def append_data(filename):
-    clipboard_data = pyperclip.paste()
-    lines = clipboard_data.strip().splitlines()
-    num_rows = len(lines)
+    y_cors = [337, 347, 357, 367, 377, 387, 397, 407, 417, 427, 437, 447, 457, 467, 477]
+    clipboard_data = ""
+    num_rows = 0
+    for y_cor in y_cors:
+        copy(y_cor)
+        clipboard_data = pyperclip.paste()
+        lines = clipboard_data.strip().splitlines()
+        num_rows = len(lines)
+
+        if num_rows == 10:
+            break
+
     if num_rows != 10:
         clipboard_data = f"Error: {filename}, {num_rows}lines\n"
     with open("data/squad/plausible_translated.jsonl", "a", encoding="utf-8") as f:
@@ -56,7 +85,7 @@ def delete_temp_files():
             print(f"Deleted: {file_path}")
 
 
-def copy_file():
+def copy_file(filename):
     if os.path.isfile(source_path):
         dest_path = os.path.join(destination_folder, filename)
         shutil.copy2(source_path, dest_path)
@@ -73,14 +102,18 @@ destination_folder = "temp/"
 
 
 files = os.listdir(source_folder)
-files = sorted(files, key=extract_number)[265:500]
+files = sorted(files, key=extract_number)[1296:]
 is_first = True
 for filename in files:
     source_path = os.path.join(source_folder, filename)
 
     delete_temp_files()
-    copy_file()
-    upload_file(is_first, filename)
+    copy_file(filename)
+    # upload_file(is_first, filename)
+    fill_file(is_first, filename)
     append_data(filename)
+
+    pyautogui.moveTo(660, 890, duration=0.5)
+    pyautogui.click()
 
     is_first = False
