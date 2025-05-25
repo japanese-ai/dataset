3d93234271179867fdd7b034bc2faadb410da5a8
WaiYanMK <waiyanminkhaing1997@gmail.com>
2025-05-25 14:04:08 +0900

    save plausible

--- src/script/squad/get_answer.py
+++ src/script/squad/get_answer.py
@@ -2,7 +2,7 @@ from script.squad.answer import Answer
 
 answer = Answer()
 answer.have_graph_data = True
-answer.start_get_data(5990
+answer.start_get_data(6017
                       , start_from_new_chat=False
-                      , start_from_half=False
+                      , start_from_half=True
                       )
