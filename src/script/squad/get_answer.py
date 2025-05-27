4a0fd9f0a3d8f7faf039ca9960bb1e72066fa6f0
WaiYanMK <waiyanminkhaing1997@gmail.com>
2025-05-27 10:54:42 +0900

    save answer

--- src/script/squad/get_answer.py
+++ src/script/squad/get_answer.py
@@ -2,4 +2,4 @@ from script.squad.answer import Answer
 
 answer = Answer()
 answer.have_graph_data = True
-answer.start_get_data(62715, start_from_new_chat=False)
+answer.start_get_data(63230, start_from_new_chat=False)
