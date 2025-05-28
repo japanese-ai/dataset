9b69d0f5308be2cd00d2566e66a6aeff1b418acf
Wai Yan Min Khaing <waiyanminkhaing1997@gmail.com>
2025-05-28 17:24:00 +0900

    save fixed plausible answer

--- src/script/squad/get_plausible_answer.py
+++ src/script/squad/get_plausible_answer.py
@@ -5,16 +5,16 @@ plausible = PlausibleAnswer()
 # plausible.add_no_to_output()
 plausible.have_no = True
 
-# invalid_list = plausible.extract_invalid_data_from_output(
-#     "temp/plausible_invalid_data.jsonl"
-# )
-# print(f"Invalid data count: {len(invalid_list)}")
-
-plausible.fix_invalid_data(
-    invalid_file="temp/plausible_invalid_data.jsonl",
-    fix_file="temp/plausible_invalid_fixed_data.jsonl",
-    start_from_new_chat=False,
+invalid_list = plausible.extract_invalid_data_from_output(
+    "temp/plausible_invalid_data.jsonl"
 )
+print(f"Invalid data count: {len(invalid_list)}")
+
+# plausible.fix_invalid_data(
+#     invalid_file="temp/plausible_invalid_data.jsonl",
+#     fix_file="temp/plausible_invalid_fixed_data.jsonl",
+#     start_from_new_chat=False,
+# )
 
 # plausible.merge_fixed_data(
 #     fixed_file="temp/plausible_invalid_fixed_data.jsonl",
