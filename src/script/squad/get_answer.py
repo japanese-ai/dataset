6e2fe69211270933e91caaf718c890fb8bbfc3f8
WaiYanMK <waiyanminkhaing1997@gmail.com>
2025-06-02 10:15:51 +0900

    save  answer

--- src/script/squad/get_answer.py
+++ src/script/squad/get_answer.py
@@ -2,5 +2,5 @@ from script.squad.answer import Answer
 
 answer = Answer()
 answer.new_chat_target_y_cor = 350
-answer.start_get_data(69180 , start_from_new_chat=False)
+answer.start_get_data(72995, start_from_new_chat=False)
 # answer.get_error_data(start_from_new_chat=False)
